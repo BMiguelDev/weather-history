de0355103a3d804835b50abbc212c86a12975509
Bruno Miguel <113399299+BMiguelDev@users.noreply.github.com>
2024-02-26 22:03:41 +0000

    Add bar chart displaying weather information
    - Add "chart.js" and "react-chartjs-2" dependencies for generating the bar graph
    - Add WeatherEntry typing for each entry retrieved from our backend
    - Add bar chart to application main component, which renders a bar chart based on the weather entries returned by our backend
    - Improve app's color palette
    - Add Button component for modulating button elements
    - Improve UI layout and styling
    - Add functionality to only allow submission of form when there's valid coordinates to query the backend

--- weather-history-client/src/components/WeatherHistory/WeatherHistory.tsx
+++ weather-history-client/src/components/WeatherHistory/WeatherHistory.tsx
@@ -2,16 +2,20 @@ import { useEffect, useRef, useState } from "react";
 import axios, { AxiosError } from "axios";
 import dayjs from "dayjs";
 import { DatePicker } from "@mui/x-date-pickers";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-import { Coordinates, LocationItem } from "../../models/model";
+import { Coordinates, LocationItem, WeatherEntry } from "../../models/model";
+import Button from "../Button/Button";
 import styles from "./WeatherHistory.module.scss";
 
 const DEBOUNCE_TIME = 300;
 const TIME_INTERVAL_DAYS = 7;
 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 const WeatherHistory = () => {
-    const [data, setData] = useState<any>([]);
-    const [errorMessage, setErrorMessage] = useState<string>("");
+    const [weatherEntries, setWeatherEntries] = useState<WeatherEntry[]>([]);
 
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [isLoadingCoordinates, setIsLoadingCoordinates] = useState<boolean>(false);
@@ -26,7 +30,7 @@ const WeatherHistory = () => {
     });
 
     const inputRef = useRef<HTMLInputElement>(null);
-    const isFormSubmittable = inputValue && startDate && endDate;
+    const isFormSubmittable = inputValue && startDate && endDate && inputCoordinates.latitude && inputCoordinates.longitude;
 
     // When <inputValue> changes, query Meteo API for an array of locations (including latitude and longitude) correspondent to the location string inputed
     useEffect(() => {
@@ -37,7 +41,6 @@ const WeatherHistory = () => {
                 latitude: "",
                 longitude: "",
             });
-            setErrorMessage("");
             setIsLoadingCoordinates(false);
             return;
         }
@@ -57,14 +60,12 @@ const WeatherHistory = () => {
                         longitude: locations[0].longitude.toString(),
                     });
                     console.log(locations);
-                    setErrorMessage("");
                 } else {
                     console.log("No locations found");
                     setInputCoordinates({
                         latitude: "",
                         longitude: "",
                     });
-                    setErrorMessage("Not Locations Found");
                 }
             } catch (err) {
                 console.log("Error retrieving coordinates from Geocoding API");
@@ -73,7 +74,6 @@ const WeatherHistory = () => {
                     latitude: "",
                     longitude: "",
                 });
-                setErrorMessage("Error retrieving coordinates from Geocoding API");
             } finally {
                 setIsLoadingCoordinates(false);
             }
@@ -83,24 +83,23 @@ const WeatherHistory = () => {
         return () => clearTimeout(getCoordinates);
     }, [inputValue, setInputCoordinates]);
 
-
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        if (!inputValue || !startDate || !endDate) return;
+        if (!isFormSubmittable) return;
 
-        setIsLoading(true)
+        setIsLoading(true);
 
         try {
             const response = await axios.get(
                 `http://localhost:3001/api/weather_entries?latitude=${inputCoordinates.latitude}&longitude=${inputCoordinates.longitude}&start_date=${startDate}&end_date=${endDate}`
             );
             console.log(response.data);
-            setData(response.data);
+            setWeatherEntries(response.data);
         } catch (err) {
             if (err instanceof AxiosError) {
                 if (err.response?.status === 422) {
                     console.log("Incorrect query parameters");
-                    setErrorMessage(err.response?.data.error);
+                    console.log(err.response?.data.error);
                 } else {
                     console.log(err);
                 }
@@ -128,10 +127,72 @@ const WeatherHistory = () => {
         }
     };
 
+    const backgroundColors = ["rgb(43, 105, 134)", "rgb(16, 41, 51)", "rgb(14, 100, 139)", "rgb(23, 174, 244)"];
+    const chartData = {
+        labels: weatherEntries.map((weather_entry: WeatherEntry) => weather_entry.date),
+        datasets: [
+            {
+                label: "Max Temp",
+                data: weatherEntries.map((weather_entry: WeatherEntry) => weather_entry.max_temp),
+                backgroundColor: backgroundColors[0],
+                hoverBackgroundColor: "rgb(150, 210, 195)",
+                hoverBorderColor: "rgb(18, 18, 18)",
+                borderWidth: 1,
+            },
+            {
+                label: "Min Temp",
+                data: weatherEntries.map((weather_entry: WeatherEntry) => weather_entry.min_temp),
+                backgroundColor: backgroundColors[1],
+                hoverBackgroundColor: "rgb(150, 210, 195)",
+                hoverBorderColor: "rgb(18, 18, 18)",
+                borderWidth: 1,
+            },
+            {
+                label: "Precipitation",
+                data: weatherEntries.map((weather_entry: WeatherEntry) => weather_entry.precipitation),
+                backgroundColor: backgroundColors[2],
+                hoverBackgroundColor: "rgb(150, 210, 195)",
+                hoverBorderColor: "rgb(18, 18, 18)",
+                borderWidth: 1,
+            },
+            {
+                label: "Wind",
+                data: weatherEntries.map((weather_entry: WeatherEntry) => weather_entry.wind_speed),
+                backgroundColor: backgroundColors[3],
+                hoverBackgroundColor: "rgb(150, 210, 195)",
+                hoverBorderColor: "rgb(18, 18, 18)",
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const chartOptions = {
+        plugins: {
+            title: {
+                display: true,
+                text: inputValue,
+                padding: {
+                    bottom: 10,
+                },
+                font: {
+                    size: 20,
+                },
+            },
+            tooltip: {
+                callbacks: {
+                    label: function (data: any) {
+                        return data.dataset.label + ": " + data.formattedValue;
+                    },
+                },
+            },
+            maintainAspectRatio: true,
+        },
+    };
+
     return (
         <main className={styles.main_container}>
-            <form onSubmit={handleSubmit}>
-                <div className={inputValue.length >= 50 ? "input_max_limit" : ""}>
+            <form onSubmit={handleSubmit} className={styles.form_container}>
+                <div className={styles.input_container}>
                     <label htmlFor="inputValue">Location:</label>
                     <div>
                         <input
@@ -146,29 +207,45 @@ const WeatherHistory = () => {
                         />
                     </div>
                 </div>
-                <DatePicker
-                    label="Start Date"
-                    value={dayjs(startDate)}
-                    onChange={(e) => handleChangeStartDate(e)}
-                    minDate={dayjs("1940-01-01")}
-                    maxDate={dayjs(endDate)}
-                />
-                <DatePicker
-                    label="End Date"
-                    value={dayjs(endDate)}
-                    onChange={(e) => setEndDate(e?.format("YYYY-MM-DD"))}
-                    minDate={startDate ? dayjs(startDate) : dayjs("1940-01-01")}
-                    maxDate={dayjs(startDate).add(TIME_INTERVAL_DAYS, "day")}
-                />
-                <button type="submit" disabled={!isFormSubmittable}>
-                    {isLoadingCoordinates ? "Loading..." : "Submit"}
-                </button>
+                <div className={styles.date_inputs_container}>
+                    <div className={styles.start_date_container}>
+                        <DatePicker
+                            label="Start Date"
+                            value={dayjs(startDate)}
+                            onChange={(e) => handleChangeStartDate(e)}
+                            minDate={dayjs("1940-01-01")}
+                            maxDate={dayjs(endDate)}
+                        />
+                    </div>
+                    <div className={styles.start_date_container}>
+                        <DatePicker
+                            label="End Date"
+                            value={dayjs(endDate)}
+                            onChange={(e) => setEndDate(e?.format("YYYY-MM-DD"))}
+                            minDate={startDate ? dayjs(startDate) : dayjs("1940-01-01")}
+                            maxDate={dayjs(startDate).add(TIME_INTERVAL_DAYS, "day")}
+                        />
+                    </div>
+                </div>
+                <div className={styles.button_container}>
+                    <Button
+                        buttonBgColor="rgb(38, 136, 188)"
+                        buttonTitle={isLoadingCoordinates ? "Loading..." : "Submit"}
+                        buttonType="submit"
+                        isDisabled={!isFormSubmittable}
+                    />
+                </div>
             </form>
 
-            <br />
-            <div>{data.length !== 0 ? JSON.stringify(data) : "Loading"}</div>
-            <br />
-            {errorMessage !== "" ? <div>{errorMessage}</div> : ""}
+            <div className={styles.chart_container}>
+                {weatherEntries.length > 0 && !isLoading ? (
+                    <Bar data={chartData} options={chartOptions} />
+                ) : weatherEntries.length > 0 && isLoading ? (
+                    <h4 style={{ fontSize: "1.8rem" }}>Loading...</h4>
+                ) : (
+                    <h4 style={{ fontSize: "1.5rem" }}>No entries</h4>
+                )}
+            </div>
         </main>
     );
 };
